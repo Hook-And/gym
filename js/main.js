4d592909f6de0e7c2063d99206a27477f95f600a
Hirumann <danil13040562@gmail.com>
2022-07-29 00:15:30 +0300

    Revert "Случайное удаление"
    This reverts commit b89c1e97981f0d33fc62ac4862a2a586b70a2113.

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let firstNumber
+let secondNumber
+
+function sum(num1, num2) {
+    const equal = num1 + num2
+    console.log(equal)
+}
+
+firstNumber = 54
+secondNumber = 128
+
+sum(firstNumber, secondNumber)
