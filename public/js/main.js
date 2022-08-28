ce3cb1dd1e72721cc9aea9be299854779987a3a5
Hook-And <osya_stalin_1878@mail.ru>
2022-08-29 02:11:44 +0300

    b101 second code review

--- public/js/main.js
+++ public/js/main.js
@@ -3,18 +3,23 @@ $(function (){
     const leftArrow = $('.left__arrow')
     const rightArrow = $('.right__arrow')
     let images = ["infobg.jpg", "1.png", "2.png", "3.png", "4.png"]
+    const url = "../assets/"
 
     leftArrow.click(() => {
-        images.unshift(images[images.length - 1])
-        images.pop()
-        info.css("background-image", "linear-gradient(0deg, rgb(30, 30, 30), rgba(30, 30, 30, 0)), url(\"../assets/" + images[0] + "\")")
+        images = changeImage(images.length - 1, info, images, url)
     });
 
     rightArrow.click(() => {
-        images.push(images[0])
-        images.shift()
-        info.css("background-image", "linear-gradient(0deg, rgb(30, 30, 30), rgba(30, 30, 30, 0)), url(\"../assets/" + images[0] + "\")")
+        images = changeImage(0, info, images, url)
     })
+
+    function changeImage(elementPosition, htmlElement, imagesArray, url) {
+        const tempElement = imagesArray.splice(elementPosition, 1)
+        imagesArray.splice(imagesArray.length - elementPosition, 0, tempElement)
+        htmlElement.css("background-image", "linear-gradient(0deg, rgb(30, 30, 30), rgba(30, 30, 30, 0)), url(\"" + url + imagesArray[0] + "\")")
+
+        return imagesArray
+    }
 })
 
 
