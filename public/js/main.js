e5001cdc42093481bc6a6f561e8bf31f0e347be4
Hook-And <osya_stalin_1878@mail.ru>
2022-08-29 01:13:32 +0300

    b101 rough code review

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,19 @@
 $(function (){
-    let currentPosition = 0
     const info = $('.intro')
     const leftArrow = $('.left__arrow')
     const rightArrow = $('.right__arrow')
-    const length = 5
-
-    function switchPosition(Position) {
-        switch (Position) {
-            case 0:
-                info.removeClass('js-new-intro-1').removeClass('js-new-intro-2').removeClass('js-new-intro-3').removeClass('js-new-intro-4').addClass('js-new-intro-0')
-                break
-            case 1:
-                info.removeClass('js-new-intro-0').removeClass('js-new-intro-2').removeClass('js-new-intro-3').removeClass('js-new-intro-4').addClass('js-new-intro-1')
-                break
-            case 2:
-                info.removeClass('js-new-intro-1').removeClass('js-new-intro-0').removeClass('js-new-intro-3').removeClass('js-new-intro-4').addClass('js-new-intro-2')
-                break
-            case 3:
-                info.removeClass('js-new-intro-1').removeClass('js-new-intro-2').removeClass('js-new-intro-0').removeClass('js-new-intro-4').addClass('js-new-intro-3')
-                break
-            case 4:
-                info.removeClass('js-new-intro-1').removeClass('js-new-intro-2').removeClass('js-new-intro-3').removeClass('js-new-intro-0').addClass('js-new-intro-4')
-                break
-            default:
-                break
-        }
-    }
+    let images = ["infobg.jpg", "1.png", "2.png", "3.png", "4.png"]
 
     leftArrow.click(() => {
-        if (currentPosition === 0) {
-            currentPosition = length - 1
-        } else {
-            currentPosition -= 1
-        }
-        info.hide()
-        switchPosition(currentPosition)
-        info.fadeIn(500)
-    })
+        images.unshift(images[images.length - 1])
+        images.pop()
+        info.css("background-image", "linear-gradient(0deg, rgb(30, 30, 30), rgba(30, 30, 30, 0)), url(\"../assets/" + images[0] + "\")")
+    });
+
     rightArrow.click(() => {
-        if(currentPosition === length - 1) {
-            currentPosition = 0
-        }
-        else {
-            currentPosition += 1
-        }
-        info.hide()
-        switchPosition(currentPosition)
-        info.fadeIn(500)
+        images.push(images[0])
+        images.shift()
+        info.css("background-image", "linear-gradient(0deg, rgb(30, 30, 30), rgba(30, 30, 30, 0)), url(\"../assets/" + images[0] + "\")")
     })
 })
 
